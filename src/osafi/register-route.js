dcf8d40fbec98801c6021d6e798855cc90746c97
Omeed Safi <omeed@safi.ms>
2020-05-23 19:08:38 -0700

    Initial commit

---
+++ src/osafi/register-route.js
@@ -0,0 +1,48 @@
+var room = HBInit();
+
+room.pluginSpec = {
+  name: `osafi/register-route`,
+  author: `osafi`,
+  version: `1.0.0`,
+  config: {
+    directorRegistrationUrl: 'http://localhost:8080/routes',
+    username: '',
+    password: '',
+    route: ''
+  },
+  configDescriptions: {},
+  dependencies: [],
+  order: {},
+  incompatible_with: [],
+}
+
+const config = room.pluginSpec.config;
+
+async function onRoomLinkHandler(url) {
+  const authorization = `Basic ${btoa(config.username + ":" + config.password)}`
+  try {
+    const response = await fetch(config.directorRegistrationUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': authorization
+      },
+      body: JSON.stringify({
+        path: config.route,
+        target: url
+      })
+    });
+
+    if (!response.ok) {
+      const body = await response.text()
+      throw new Error(`Unable to register room URL with director: ${response.status} - ${response.statusText} - ${body}`)
+    }
+    
+    room.log('Registered room url with director', HHM.log.level.INFO);
+  } catch(error) {
+    room.log(error.message, HHM.log.level.ERROR);
+  }
+}
+
+
+room.onRoomLink = onRoomLinkHandler;
